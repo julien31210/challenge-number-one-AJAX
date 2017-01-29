987aa16eddc75b24adf2d58dd4f72740eaf9fd9d
Julien <julien.linder@gmail.com>
2017-01-29 23:52:37 +0100

    code non fonctionnel

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
 $(document).ready(function(){
+	var y;
+	var tabnum = [];
 	$.ajax({
 		type:"GET",
 		url:"xml.xml",
 		dataType:"xml",
 		success: function(xml){ 
-	$(xml).find('xml').each(function(){
-    	var id = $(this).attr('key');
-    	console.log("key")
-	    	var client = $(this).find('client').text("0");
-	    	var url = $(this).find('url').text("client");
-		});
-	}
+			$(xml).find('client').each(function(){
+				y = $(this).attr('id')
+				tabnum.push(y);
+				nom = $(this).find('nom').text();
+				$('#Div_XML').append($('<option>', {value:y, text: nom}));
+				console.log((nom));
+			});
+				$("select").change(function(){
+				client = $("option").data('id');
+				$ (".reception").html([client])
+				console.log(client)
+			});
+		}
+	});
 });
-});
+
+
+				//console.log(x[0])
+				// $(this).find("client").each(function(){
+				// 	console.log($(this).find("nom").html())
+					// console.log($(this).find("prenom").html())
+					// console.log($(this).find("âge").html())
+					// console.log($(this).find("profession").html())
+					// console.log($(this).find("email").html())
+					// console.log($(this).find("téléphone").html())
+
+
+
+
+			//});
+
+
+// 				$(".reception").each(function(){
+// 					var x= $(this).find("xml");
+// 					console.log(this);
+// 				})
+// 			})
+// 		}
+// 	});
+
+// });
