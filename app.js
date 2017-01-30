5348b5016d2700b574e8bdddbadd24454b51d532
Julien <julien.linder@gmail.com>
2017-01-30 15:26:15 +0100

    code javascript marche enfin

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ $(document).ready(function(){
 		url:"xml.xml",
 		dataType:"xml",
 		success: function(xml){ 
-			$(xml).find('client').each(function(){
+
+		var client_list = $(xml).find('client');
+
+			client_list.each(function(){
 				y = $(this).attr('id')
 				tabnum.push(y);
 				nom = $(this).find('nom').text();
@@ -14,15 +17,30 @@ $(document).ready(function(){
 				console.log((nom));
 			});
 				$("select").change(function(){
-				client = $("option").data('id');
-				$ (".reception").html([client])
+				//client = $("option").data('id');
+				var client_id = $( "#Div_XML option:selected" ).val();
+				client = client_list[client_id],
+				$ (".reception").html([client]),
 				console.log(client)
 			});
 		}
 	});
 });
 
-
+//$(document).ready(function(){
+//	$.ajax({
+//		type:"GET",
+//		url:"xml.xml",
+//		dataType:"xml",
+//		success: function(xml){ 
+//				var id = $(this).attr('key');
+//				console.log("key")
+//				var client = $(this).find('client').text("0");
+//				var url = $(this).find('url').text("client");
+//			});
+//		}
+//	});
+//}); 
 				//console.log(x[0])
 				// $(this).find("client").each(function(){
 				// 	console.log($(this).find("nom").html())
